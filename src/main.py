2377c0e40a50b79a09999d847fb00540ede56594
Pedro Martins Trevisan <pedrotrevs@gmail.com>
2025-09-02 14:22:02 -0400

    feat: implement weather and time retrieval tools, update agent execution in main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,27 @@
 import os
 import dotenv
-from langchain_community.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
+from langchain_core.tools import tool
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.agents import AgentExecutor, create_tool_calling_agent
+
+@tool
+def get_current_weather(city: str) -> str:
+    "Busca o clima de uma cidade."
+    return f"The weather in {city} is sunny"
+
+def get_current_time(city: str) -> str:
+    "Busca o horário de uma cidade."
+    return f"The time in {city} is 10:00"
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "Você é um assistente que ajuda com informações sobre clima."),
+    ("human", "{input}"),
+    ("placeholder", "{agent_scratchpad}"),  # ← Esta linha é obrigatória!
+])
+
+tools = [get_current_weather]
+
 
 dotenv.load_dotenv()
 
@@ -11,11 +32,12 @@ if not OPENAI_API_KEY:
 
 llm = ChatOpenAI(model="gpt-4o-mini", api_key=OPENAI_API_KEY)
 
+agent = create_tool_calling_agent(llm, tools, prompt)
+executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
-def main():
-    response = llm.invoke("Quanto é 2 + 2?")
-    print(response.content)
+res = executor.invoke({"input": "Qual o clima de São Paulo? E qual o horário de São Paulo?"})
+print(res["output"])
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#    main()
